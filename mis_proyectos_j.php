61f3e6fc931d34cc8c882f9bec621ddb3e33dfb9
CesarGMtz <93628856+CesarGMtz@users.noreply.github.com>
2023-04-23 21:32:20 -0600

    Update mis_proyectos_j.php

--- mis_proyectos_j.php
+++ mis_proyectos_j.php
@@ -1,50 +1,84 @@
 <?php
 session_start();
+$user = $_SESSION["Usuario"];
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="css/mis_proyectos_j.css">
+   <link rel="stylesheet" href="css/autorizar.css">
 </head>
 
+<section id='steezy'>
+
 <body>
+
+<section class="w3-threequarter w3-padding-large w3-right"> <!--NO FUNCIONA BIEN EL SIDEBAR-->
+        <!--DESKTOP NAVIGATION-->
+        <div class="w3-container w3-padding-large w3-border-bottom w3-hide-small">
+          
+          <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="pagina_inicio_j.php" class="center">Pagina de Inicio</a>
+            <a href="#" class="center">Mis Proyectos</a> 
+            <a href="proyectos1.php" class="center">Proyectos</a> 
+            <a href="about.html" class="center">About</a>
+          </div>
+
+          <span style="font-size:30px;cursor:pointer" onclick="openNav()" class="center">&#9776; Menu</span>
+
 <section>
   <h1>Projects</h1>
 
-  <div class="container">
+   <table>
+    <thead>
+      <tr>
+        <td>
+          <h1>Id del Proyecto</h1>
+        </td>
+        <td>
+          <h3>Area Estrategica</h3>
+        </td>
+        <td>
+          <h3>Unidad de Formacion</h3>
+        </td>
+        <td>
+          <h3>Nivel de Desarrollo</h3>
+        </td>
+        <td>
+          <h3>Nombre</h3>
+        </td>
+        <td>
+          <h3>Autorizacion</h3>
+        </td>
+      </tr>
+    </thead>
+
+    <tbody>
   
   <?php
 								   	include 'database.php';
 								   	$pdo = Database::connect();
-								   	$sql = 'SELECT * FROM R_Proyecto natural join R_Edicion natural join R_Area_Estrategica natural join R_Unidad_Formacion natural join R_Nivel_Desarrollo ORDER BY Id_Proyecto';
+								   	$sql = 'SELECT * FROM R_Juez NATURAL JOIN R_Juez_Proyecto_Calif JOIN R_Proyecto NATURAL JOIN R_Area_Estrategica NATURAL JOIN R_Unidad_Formacion NATURAL JOIN R_Nivel_Desarrollo WHERE R_Juez_Proyecto_Calif.Id_Proyecto = R_Proyecto.Id_Proyecto AND Id_Juez ="' . $user . '"';
 				 				   	foreach ($pdo->query($sql) as $row) {
-				 				   	echo '<div class="item" id=' .$row['Id_Proyecto'] . '>';
-								      	echo '<div class="text">';
-										echo '<h3>'. $row['NombrePy'] . '</h3>';
-										echo '<p>'. $row['Id_Proyecto'] . '</p>';
-									  	echo '<p>'. $row['Nivel'] . '</p>';
-								      	echo '</div>';
-								      	echo '<div class="button">View More</div>';
-								    	echo '</div>'; #SOLO IMPRIME 2 PROYECTOS DE LOS 8
-									
+				 				   	echo '<tr>';
+                                        				echo '<td>'. $row['Id_Proyecto'] . '</td>';
+			    					   	echo '<td>'. $row['NombreAe'] . '</td>';
+			    					   	echo '<td>'. $row['NombreUf'] . '</td>';
+			    					  	echo '<td>'. $row['Nivel'] . '</td>';
+                                        				echo '<td>'. $row['NombrePy'] . '</td>';
+			                            echo '<td width=250>';
+			    					   	echo '<a class="btn" href="detalles.php?id='.$row['Id_Proyecto'].'">Detalles</a>';
+			    					   	echo '&nbsp;';
+			    					  	echo '<a class="btn btn-success" href="calificar-j.php?id='.$row['Id_Proyecto'].'">Calificar</a>';
+			    					   	echo '&nbsp;';
+			    					   	echo '</td>';
+										  echo '</tr>';
 								    }
 								   	Database::disconnect();
 				  				?>
-				  				
-    <div class="item" id="1"> <!--LO DE ARRIBA DEPENDE DE ESTO-->
-      <img src="https://www.enter.co/wp-content/uploads/2017/10/robotica-en-sofa-2017.jpg" alt="">
-      <div class="text">
-        <h3>PROJECT 1</h3>
-          <p>Short Description</p>
-      </div>
-      <div class="button">View More</div>
-    </div>
 
-
-
-
-  <!-- Modal -->
+  <!-- Modal 
   <div id="preview" class="modal">
     <div class="modal-content">
       <span class="close">&times;</span>
@@ -55,7 +89,7 @@ session_start();
         <div class="button" id="live">Calificar</div>
       </div>
     </div>
-  </div>
+  </div> -->
 </section>
   <script src="JavaScript/mis_proyectos_j.js"></script>
 </body>
