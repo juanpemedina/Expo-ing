4708b146035bd138581d24edb46d5a97201a6164
sansantoss <88722326+sansantoss@users.noreply.github.com>
2023-04-24 16:08:15 -0600

    Update database.php

--- database.php
+++ database.php
@@ -1,14 +1,14 @@
 <?php
 	class Database {
-		//private static $dbName 		= 'TC2005B_401_2' ;
-		//private static $dbHost 		= 'localhost' ;
-		//private static $dbUsername 	= 'TC2005B_401_2';
-		//private static $dbUserPassword 	= 'S9BrLkLP*utl8UTl';
-		
-		private static $dbName 		= 'Pagina_Web' ;
+		private static $dbName 		= 'TC2005B_401_2' ;
 		private static $dbHost 		= 'localhost' ;
-		private static $dbUsername 	= 'root';
-		private static $dbUserPassword 	= 'CMar 22! 74!';
+		private static $dbUsername 	= 'TC2005B_401_2';
+		private static $dbUserPassword 	= 'S9BrLkLP*utl8UTl';
+		
+		//private static $dbName 		= 'Pagina_Web' ;
+		//private static $dbHost 		= 'localhost' ;
+		//private static $dbUsername 	= 'root';
+		//private static $dbUserPassword 	= 'CMar 22! 74!';
 
 		private static $cont  = null;
 
