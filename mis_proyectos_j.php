76e03aed3a9b3696f09837438344b3ac881d4c02
CesarGMtz <93628856+CesarGMtz@users.noreply.github.com>
2023-04-30 00:11:56 -0600

    Update mis_proyectos_j.php

--- mis_proyectos_j.php
+++ mis_proyectos_j.php
@@ -1,12 +1,24 @@
 <?php
+
 session_start();
-$id = $_SESSION["TipoUsuario"];
+$key = $_SESSION["TipoUsuario"];
 $user = $_SESSION["Usuario"];
+
+if ( $key!=1 and $key!=2 and $key!=3 and $key!=4) {
+		header("Location: 404.html");
+	}
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+	<title>Mis Proyectos</title>
+  <meta charset="UTF-8" />
+<!--Montserrat font-->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/pagina_inicio.css" />
+    <script src="JavaScript/menu.js"></script> 
    <link rel="stylesheet" href="css/autorizar.css">
 </head>
 
@@ -14,22 +26,40 @@ $user = $_SESSION["Usuario"];
 
 <body>
 
-<section class="w3-threequarter w3-padding-large w3-right"> <!--NO FUNCIONA BIEN EL SIDEBAR-->
+<section class="w3-threequarter w3-padding-large w3-right">
         <!--DESKTOP NAVIGATION-->
         <div class="w3-container w3-padding-large w3-border-bottom w3-hide-small">
           
           <div id="mySidenav" class="sidenav">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <a href="pagina_inicio_j.php" class="center">Pagina de Inicio</a>
-            <a href="#" class="center">Mis Proyectos</a> 
-            <a href="proyectos1.php" class="center">Proyectos</a> 
+            
+            <?php
+            if ($key == 1) {
+            	echo '<a href="pagina_inicio_e.php" class="center">Pagina de Inicio</a>';
+            	echo '<a href="registro.php" class="center">Registrar</a>';
+            }
+            else if ($key == 2) {
+            	echo '<a href="pagina_inicio_p.php" class="center">Pagina de Inicio</a>';
+            }
+            else if ($key == 3) {
+            	echo '<a href="pagina_inicio_j.php" class="center">Pagina de Inicio</a>';
+            }
+            else {
+            	echo '<a href="pagina_inicio_a.php" class="center">Pagina de Inicio</a>';
+            }
+            
+            ?>
+            
+            <a href="proyectos1.php" class="center">Proyectos</a>
             <a href="about.html" class="center">About</a>
+            <a href="logout.php" class="center">Cerrar Sesión</a>
           </div>
 
-          <span style="font-size:30px;cursor:pointer" onclick="openNav()" class="center">&#9776; Menu</span>
+          <span style="font-size:30px;cursor:pointer" onclick="openNav()" class="left">&#9776; Menu</span>
+	</div>
+</section>
 
-<section>
-  <h1>Projects</h1>
+  <h1>Mis Proyectos</h1>
 
    <table>
     <thead>
@@ -61,13 +91,13 @@ $user = $_SESSION["Usuario"];
 								   	include 'database.php';
 								   	$pdo = Database::connect();
 								   	
-								   	if ($id == 1) {
+								   	if ($key == 1) {
 								   	$sql = 'SELECT * FROM R_Alumno NATURAL JOIN R_Alumno_Proyecto JOIN R_Proyecto NATURAL JOIN R_Area_Estrategica NATURAL JOIN R_Unidad_Formacion NATURAL JOIN R_Nivel_Desarrollo WHERE R_Alumno_Proyecto.Id_Proyecto = R_Proyecto.Id_Proyecto AND Matricula ="' . $user . '"';
 								   	}
-								   	else if ($id == 2) {
+								   	else if ($key == 2) {
 								   	$sql = 'SELECT * FROM R_Profesor NATURAL JOIN R_Proyecto NATURAL JOIN R_Area_Estrategica NATURAL JOIN R_Unidad_Formacion NATURAL JOIN R_Nivel_Desarrollo WHERE Nomina ="' . $user . '"';
 								   	}
-								   	else if ($id == 3) {
+								   	else if ($key == 3) {
 								   	$sql = 'SELECT * FROM R_Juez NATURAL JOIN R_Juez_Proyecto_Calif JOIN R_Proyecto NATURAL JOIN R_Area_Estrategica NATURAL JOIN R_Unidad_Formacion NATURAL JOIN R_Nivel_Desarrollo WHERE R_Juez_Proyecto_Calif.Id_Proyecto = R_Proyecto.Id_Proyecto AND Id_Juez ="' . $user . '" AND Autorizacion = 1';
 								   	$rQ = $pdo->query($sql);
 								   	$nQ = $rQ -> rowCount();
@@ -84,15 +114,21 @@ $user = $_SESSION["Usuario"];
 			    					   	echo '<td>'. $row['NombreUf'] . '</td>';
 			    					  	echo '<td>'. $row['Nivel'] . '</td>';
                                         				echo '<td>'. $row['NombrePy'] . '</td>';
-			                            echo '<td width=250>';
+                                        				echo '<td>';    echo ($row['Autorizacion'])?"SI":"NO"; echo'</td>';
+			                            		echo '<td width=150>';
 			    					   	echo '<a class="btn" href="detalles.php?id='.$row['Id_Proyecto'].'">Detalles</a>';
 			    					   	echo '&nbsp;';
+			    					echo '<td width=150>';
 			    					   	
-			    					   	if ($id == 2) {
+			    					   	if ($key == 1) {
+			    					   	echo '<a class="btn btn-success" href="#?id='.$row['Id_Proyecto'].'">Actualizar</a>';
+			    					   	echo '&nbsp;';
+			    					   	}
+			    					   	else if ($key == 2) {
 			    					   	echo '<a class="btn btn-success" href="autorizar_py.php?id='.$row['Id_Proyecto'].'">Autorizar</a>';
 			    					   	echo '&nbsp;';
 			    					   	}
-			    					   	else if ($id == 3) {
+			    					   	else if ($key == 3) {
 			    					  	echo '<a class="btn btn-success" href="calificar-j.php?id='.$row['Id_Proyecto'].'">Calificar</a>';
 			    					  	echo '&nbsp;';
 			    					  	}
@@ -103,18 +139,7 @@ $user = $_SESSION["Usuario"];
 								   	Database::disconnect();
 				  				?>
 
-  <!-- Modal 
-  <div id="preview" class="modal">
-    <div class="modal-content">
-      <span class="close">&times;</span>
-      <img id="img" src="">
-      <div id="details">
-        <h3 id="title"></h3>
-        <p id="info">Some text</p>
-        <div class="button" id="live">Calificar</div>
-      </div>
-    </div>
-  </div> -->
+ 
 </section>
   <script src="JavaScript/mis_proyectos_j.js"></script>
 </body>
