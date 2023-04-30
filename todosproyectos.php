a449ead0a02e7099962f9851f9ff469563abd554
CesarGMtz <93628856+CesarGMtz@users.noreply.github.com>
2023-04-29 21:28:59 -0600

    Update todosproyectos.php

--- todosproyectos.php
+++ todosproyectos.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+$key = $_SESSION["TipoUsuario"];
+$user = $_SESSION["Usuario"];
 
 	$idA = null;
 	if ( !empty($_GET['idA'])) {
@@ -15,6 +17,10 @@ session_start();
 <html>
 
 <head>
+	<!--Montserrat font-->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/pagina_inicio.css" />
+    <script src="JavaScript/menu.js"></script> 
   <link rel="stylesheet" href="css/autorizar.css">
 </head>
 
@@ -29,6 +35,42 @@ session_start();
 <section id='steezy'>
 
 <body>
+	
+<section class="w3-threequarter w3-padding-large w3-right">
+        <!--DESKTOP NAVIGATION-->
+        <div class="w3-container w3-padding-large w3-border-bottom w3-hide-small">
+          
+          <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            
+            <?php
+            if ($key == 1) {
+            	echo '<a href="pagina_inicio_e.php" class="center">Pagina de Inicio</a>';
+            	echo '<a href="registro.php" class="center">Registrar</a>';
+            	echo '"mis_proyectos_j.php" class="center">Mis Proyectos</a>';
+            }
+            else if ($key == 2) {
+            	echo '<a href="pagina_inicio_p.php" class="center">Pagina de Inicio</a>';
+            	echo '<a href="mis_proyectos_j.php" class="center">Mis Proyectos</a>';
+            }
+            else if ($key == 3) {
+            	echo '<a href="pagina_inicio_j.php" class="center">Pagina de Inicio</a>';
+            	echo '<a href="mis_proyectos_j.php" class="center">Mis Proyectos</a>';
+            }
+            else {
+            	echo '<a href="pagina_inicio_a.php" class="center">Pagina de Inicio</a>';
+            }
+            
+            ?>
+            
+            <a href="#" class="center">Proyectos</a>
+            <a href="about.html" class="center">About</a>
+          </div>
+
+          <span style="font-size:30px;cursor:pointer" onclick="openNav()" class="center">&#9776; Menu</span>
+	</div>
+</section>
+	
 <section>
   <h1>Proyectos Asignados</h1>
 
