cb902a757c71140008ad3c690e57e973776fbf07
CesarGMtz <93628856+CesarGMtz@users.noreply.github.com>
2023-04-30 01:39:00 -0600

    Update autorizar_py.php

--- autorizar_py.php
+++ autorizar_py.php
@@ -1,6 +1,12 @@
 <?php
 
 session_start();
+$key = $_SESSION["TipoUsuario"];
+$user = $_SESSION["Usuario"];
+
+if ( $key!=2 and $key!=4) {
+		header("Location: 404.html");
+	}
 
 	require 'database.php';
 
@@ -33,7 +39,7 @@ session_start();
 			$autorizacionq = ($autorizacion=="S")?1:0;
 			$q->execute(array($id,$autorizacionq, $id));
 			Database::disconnect();
-			header("Location: autorizar.php");
+			header("Location: mis_proyectos_j.php");
 		}
 	}
 	else {
@@ -55,24 +61,46 @@ session_start();
 <head>
   <title>Autorizar</title>
   <meta charset="UTF-8" />
+<!--Montserrat font-->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/pagina_inicio.css" />
+    <script src="JavaScript/menu.js"></script> 
   <link rel="stylesheet" href="css/autorizar_py.css">
 </head>
 
 <body>
 
-<section class="w3-threequarter w3-padding-large w3-right"> <!--NO FUNCIONA BIEN EL SIDEBAR-->
+<section class="w3-threequarter w3-padding-large w3-right">
         <!--DESKTOP NAVIGATION-->
         <div class="w3-container w3-padding-large w3-border-bottom w3-hide-small">
           
           <div id="mySidenav" class="sidenav">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <a href="pagina_inicio_p.php" class="center">Pagina de Inicio</a>
-            <a href="autorizar.php" class="center">Mis Proyectos</a> 
+            
+            <?php
+            if ($key == 1) {
+            	echo '<a href="pagina_inicio_e.php" class="center">Pagina de Inicio</a>';
+            	echo '<a href="registro.php" class="center">Registrar</a>';
+            }
+            else if ($key == 2) {
+            	echo '<a href="pagina_inicio_p.php" class="center">Pagina de Inicio</a>';
+            }
+            else if ($key == 3) {
+            	echo '<a href="pagina_inicio_j.php" class="center">Pagina de Inicio</a>';
+            }
+            else {
+            	echo '<a href="pagina_inicio_a.php" class="center">Pagina de Inicio</a>';
+            }
+            
+            ?>
+            
+            <a href="proyectos1.php" class="center">Proyectos</a>
             <a href="about.html" class="center">About</a>
+            <a href="logout.php" class="center">Cerrar Sesión</a>
           </div>
 
-          <span style="font-size:30px;cursor:pointer" onclick="openNav()" class="center">&#9776; Menu</span>
-
+          <span style="font-size:30px;cursor:pointer" onclick="openNav()" class="left">&#9776; Menu</span>
+	</div>
 </section>
 
   <section class="form-registro">
@@ -108,7 +136,7 @@ session_start();
 
 					  <div class="form-actions">
 						  <button type="submit" class="btn btn-success">Autorizar</button>
-						  <a class="btn" href="autorizar.php">Regresar</a>
+						  <a class="btn" href="mis_proyectos_j.php">Regresar</a>
 						</div>
 	
     </form>
