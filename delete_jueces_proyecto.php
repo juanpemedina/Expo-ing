231ad35221e156f53eb038eac5294fe0067b88f7
H3ct0r3y3s <118298542+H3ct0r3y3s@users.noreply.github.com>
2023-05-05 21:55:06 -0600

    Add files via upload

---
+++ delete_jueces_proyecto.php
@@ -0,0 +1,57 @@
+<?php
+
+session_start();
+
+$key = $_SESSION["TipoUsuario"];
+
+	if ( $key!=4) {
+		header("Location: 404.html");
+	}
+
+	require 'database.php';
+	$id = 0;
+	if ( !empty($_GET['id'])) {
+		$id = $_REQUEST['id'];
+	}
+
+	if ( !empty($_POST)) {
+		// keep track post values
+		$id = $_POST['id'];
+		// delete data
+		$pdo = Database::connect();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql1 = "delete from R_Juez_Proyecto_Calif where Id_Proyecto=?";
+		$q1 = $pdo->prepare($sql1);
+		$q1->execute(array($id));
+		Database::disconnect();
+		header("Location: jueces_proyecto.php");
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta 	charset="utf-8">
+	    <link   href=	"css/bootstrap.min.css" rel="stylesheet">
+	    <script src=	"js/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+	    <div class="container">
+	    	<div class="span10 offset1">
+	    		<div class="row">
+			    	<h3>Eliminar Relacion</h3>
+			    </div>
+
+			    <form class="form-horizontal" action="delete_jueces_proyecto.php" method="post">
+		    		<input type="hidden" name="id" value="<?php echo $id;?>"/>
+					<p class="alert alert-error">Estas seguro que quieres eliminar esta entrada ?</p>
+					<div class="form-actions">
+						<button type="submit" class="btn btn-danger">Si</button>
+						<a class="btn" href="jueces_proyecto.php">No</a>
+					</div>
+				</form>
+			</div>
+	  </div> <!-- /container -->
+	</body>
+</html>
